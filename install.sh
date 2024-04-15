78d28afdaeaa6c435a9ad2d2fafdbfb1c359d87c
Иван Vandot <vandot@ethswarm.org>
2024-04-15 17:29:33 +0200

    chore: add support for bee-localchain (#14)
    * chore: add support for bee-localchain
    
    * Update install.sh

--- install.sh
+++ install.sh
@@ -12,4 +12,9 @@ export REPO="${REPO:-ethersphere/bee}"
 export TAG="${TAG:-latest}"
 
 echo "INSTALLING BEE NODES"
+helmsman -apply -subst-env-values -f bee.yaml -target geth
+
+# wait for hardhat setup contracts job to complete
+kubectl wait --for=condition=complete job/geth-swap-setupcontracts -n "$NAMESPACE" --timeout=1200s
+
 helmsman -apply -subst-env-values -f bee.yaml
